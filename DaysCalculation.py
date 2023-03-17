ca23cd7c67312d888cd587eb73eaa4dd6e0918b2
Eniola <techcentag@gmail.com>
2023-03-17 08:15:46 -0700

    Initial commit

---
+++ DaysCalculation.py
@@ -0,0 +1,28 @@
+calculation_time_unit = 24
+name_time_unit = "hours"
+
+def days_unit(number_of_days):
+    print(number_of_days > 0)
+    if number_of_days > 0:
+        return f"{number_of_days} days are {number_of_days*calculation_time_unit} {name_time_unit}"
+    elif number_of_days == 0:
+        return "Number of days should not be zero it must be a positive number"
+
+def validate_and_execute():
+    print( type(user_input))
+    if user_input.isdigit() or user_input.isdecimal():
+        user_input_number = float(user_input)
+        days_calculations = days_unit(user_input_number)
+        print(days_calculations)
+
+    else:
+        print("your input is invalid,dont ruin my program")
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user,enter a number of days to be converted to hours !\n")
+    validate_and_execute()
+
+
+
+
+
